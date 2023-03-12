d28637d235781754eec796177e464df133176e72
hiddenstack <bhargav999reddy@gmail.com>
2023-03-13 00:02:17 +0530

    Added a console log

--- src/discord.ts
+++ src/discord.ts
@@ -44,6 +44,8 @@ Bubble.once(Events.ClientReady, async (c) => {
         Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
         { body: commandList }
       );
+
+      console.log(`Successfully loaded application (/) commands`);
     }
   } catch (error) {
     console.log(error);
